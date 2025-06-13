90d7c9eafcbba714dfe751e29d1dd5305fff9a3e
Uliana <ulipuch@mail.ru>
2025-06-13 19:43:10 +0300

    mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -20,8 +20,9 @@ private slots:
     void on_loadButton_clicked();
     void on_applyButton_clicked();
     void on_saveButton_clicked();
-    void on_resetButton_clicked();  
+    void on_resetButton_clicked();
     void on_comboBox_currentIndexChanged(int index);
+    void on_slider_valueChanged(int value);
 
 private:
     Ui::MainWindow *ui;
@@ -34,16 +35,21 @@ private:
     int saturation;
     int temperature;
     int vignette;
+    int sharpness;
+    int sepia;
+
 
     void updateImageDisplay();
     void toggleUI(bool imageLoaded);
     void applyAllFilters();
-    QImage applyBrightness(const QImage &image, int value);
-    QImage applyContrast(const QImage &image, int value);
-    QImage applyGrayscale(const QImage &image, int value);
-    QImage applySaturation(const QImage &image, int value);
-    QImage applyTemperature(const QImage &image, int value);
-    QImage applyVignette(const QImage &image, int value);
+    QImage FilterBrightness(const QImage &image, int parameter);
+    QImage FilterContrast(const QImage &image, int parameter);
+    QImage FilterGrayscale(const QImage &image, int parameter);
+    QImage FilterSaturation(const QImage &image, int parameter);
+    QImage FilterTemperature(const QImage &image, int parameter);
+    QImage FilterVignette(const QImage &image, int parameter);
+    QImage FilterSharpness(const QImage &image, int parameter);
+    QImage FilterSepia(const QImage &image, int parameter);
 };
 
-#endif 
+#endif
